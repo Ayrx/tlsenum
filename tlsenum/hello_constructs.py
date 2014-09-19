47c4043dfecd95686b1520c93717fa4c512c4e74
Terry Chia <terrycwk1994@gmail.com>
2014-09-19 12:22:55 +0800

    Add tentative construct format for ClientHello messages.

---
+++ tlsenum/hello_constructs.py
@@ -0,0 +1,43 @@
+from construct import Array, Bytes, Struct, UBInt16, UBInt32, UBInt8
+
+
+ProtocolVersion = Struct(
+    "version",
+    UBInt8("major"),
+    UBInt8("minor")
+)
+
+Random = Struct(
+    "random",
+    UBInt32("gmt_unix_time"),
+    Bytes("random_bytes", 28)
+)
+
+SessionID = Struct(
+    "session_id",
+    UBInt8("length"),
+    Bytes("session_id", lambda ctx: ctx.length)
+)
+
+CipherSuites = Struct(
+    "cipher_suites",
+    UBInt16("length"),
+    Array(lambda ctx: ctx.length / 2, Bytes("cipher_suites", 2))
+)
+
+CompressionMethods = Struct(
+    "compression_methods",
+    UBInt8("length"),
+    Array(lambda ctx: ctx.length, UBInt8("compression_methods"))
+)
+
+ClientHello = Struct(
+    "ClientHello",
+    ProtocolVersion,
+    Random,
+    SessionID,
+    CipherSuites,
+    CompressionMethods,
+    UBInt16("extensions_length"),
+    Bytes("extensions_bytes", lambda ctx: ctx.extensions_length),
+)
