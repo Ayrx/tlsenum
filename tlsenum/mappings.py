38056bedd36eac3710bf9cf105130f381a622ee7
Terry Chia <terrycwk1994@gmail.com>
2014-09-21 20:28:04 +0800

    Add list of elliptic curves.

--- tlsenum/mappings.py
+++ tlsenum/mappings.py
@@ -328,3 +328,34 @@ class ECPointFormat(Enum):
     uncompressed = 0
     ansiX962_compressed_prime = 1
     ansiX962_compressed_char2 = 2
+
+
+class ECCurves(Enum):
+    sect163k1 = 1
+    sect163r1 = 2
+    sect163r2 = 3
+    sect193r1 = 4
+    sect193r2 = 5
+    sect233k1 = 6
+    sect233r1 = 7
+    sect239k1 = 8
+    sect283k1 = 9
+    sect283r1 = 10
+    sect409k1 = 11
+    sect409r1 = 12
+    sect571k1 = 13
+    sect571r1 = 14
+    secp160k1 = 15
+    secp160r1 = 16
+    secp160r2 = 17
+    secp192k1 = 18
+    secp192r1 = 19
+    secp224k1 = 20
+    secp224r1 = 21
+    secp256k1 = 22
+    secp256r1 = 23
+    secp384r1 = 24
+    secp521r1 = 25
+    brainpoolP256r1 = 26
+    brainpoolP384r1 = 27
+    brainpoolP512r1 = 28
