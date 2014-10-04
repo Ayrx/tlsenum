9afae4e829f59f645640268a0f1019860d3c4ffa
Terry Chia <terrycwk1994@gmail.com>
2014-10-04 19:09:10 +0800

    Add test coverage for ServerHello message with Deflate compression.

--- tests/test_parse_hello.py
+++ tests/test_parse_hello.py
@@ -84,15 +84,28 @@ class TestExtensions(object):
 
 class TestServerHello(object):
     def test_parse_server_hello(self):
-        data = (
+        deflate_no = (
             b"\x16\x03\x03\x00\x2A\x02\x00\x00\x26\x03\x03\xB5\xA4\x22\x01\x18"
             b"\xC5\x71\x41\x97\x6D\xC7\x06\x14\xC0\xE5\x78\x7A\xF3\x1D\x4E\x56"
             b"\x98\xCC\x7A\x37\xAE\x6F\x1D\xC6\xF0\x78\x68\x00\xC0\x2F\x00"
         )
 
-        server_hello = ServerHello.parse_server_hello(data)
+        server_hello = ServerHello.parse_server_hello(deflate_no)
         assert server_hello.protocol_version == "1.2"
         assert server_hello.deflate is False
+        assert server_hello.cipher_suite == (
+            "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+        )
+
+        deflate_yes = (
+            b"\x16\x03\x03\x00\x2A\x02\x00\x00\x26\x03\x03\xB5\xA4\x22\x01\x18"
+            b"\xC5\x71\x41\x97\x6D\xC7\x06\x14\xC0\xE5\x78\x7A\xF3\x1D\x4E\x56"
+            b"\x98\xCC\x7A\x37\xAE\x6F\x1D\xC6\xF0\x78\x68\x00\xC0\x2F\x01"
+        )
+
+        server_hello = ServerHello.parse_server_hello(deflate_yes)
+        assert server_hello.protocol_version == "1.2"
+        assert server_hello.deflate is True
         assert server_hello.cipher_suite == (
             "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
         )
